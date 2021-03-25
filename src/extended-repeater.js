a70ea88835b499cbe31ff05adcd78504d8c40242
Maxim Maltsev <mmi3220702@gmail.com>
2021-03-26 01:52:46 +0300

    solve hanoi-tower, extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+
+  if (!options.repeatTimes) options.repeatTimes = 1;
+
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+
+  if (options.addition === undefined) options.addition = '';
+
+  if (!options.separator) options.separator = '+';
+
+  if (!options.additionSeparator) options.additionSeparator = '|';
+
+  if (typeof str !== 'string') String(str);
+
+  let add = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) +
+    options.addition;
+  return (str + add + options.separator).repeat(options.repeatTimes - 1) + str + add;
+
+}
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,11 +2,8 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    // if (arr === undefined || arr === '') {
-    //   return 0;
-    // }
     return Array.isArray(arr) ?
       1 + Math.max(...arr.map(calculateDepth)) :
       0;
-  }
+  };
 }
